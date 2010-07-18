ede668574be5e8ed38c7a5dd49899d7907aa3a46
Toshiyuki Takahashi <toshi@think.ttoshi.ddo.jp>
2010-07-18 17:43:48 +0900

    handle the selected text

--- content/tweet.js
+++ content/tweet.js
@@ -1,14 +1,24 @@
 function tweetCurrentPage(){
-	var title = document.getElementById('content').webNavigation.document.title;
-	if(title == null){
-		title = "";
-	}
-	var url = document.getElementById('content').webNavigation.currentURI.spec;
-	window.open(
-			'http://twitter.com/home?status=' +
-			encodeURIComponent(
-					title + " " + url
-			)
-	);
-	return void(0);
+    var text = "";
+
+    var title = document.getElementById('content').webNavigation.document.title;
+    if(title == null){
+	title = "";
+    }
+
+    var selection = content.getSelection().toString();
+    if (selection) {
+        text = selection;
+    } else {
+        text = title;
+    }
+
+    var url = document.getElementById('content').webNavigation.currentURI.spec;
+    window.gBrowser.selectedTab = window.gBrowser.addTab(
+	'http://twitter.com/home?status=' +
+	    encodeURIComponent(
+		text + " " + url
+	    )
+    );
+    return void(0);
 }
