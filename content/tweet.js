8ecf7f529a62894f4fa7ced62332bd2b0fcfa891
Toshiyuki Takahashi <toshi@think.ttoshi.ddo.jp>
2010-07-18 17:09:34 +0900

    first commit

---
+++ content/tweet.js
@@ -0,0 +1,13 @@
+function tweetCurrentPage(){
+	var title = document.getElementById('content').webNavigation.document.title;
+	var url = document.getElementById('content').webNavigation.currentURI.spec;
+	window.open(
+			'http://twitter.com/home?status=' +
+			encodeURIComponent(
+					title + " " + url
+			)
+	);
+	return void(0);
+}
+
+
