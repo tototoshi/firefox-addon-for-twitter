54102df2c8fd27e3ada28635bce63bb9ab501da5
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-08-25 17:28:37 +0900

    Suppress opening new window

--- content/tweet.js
+++ content/tweet.js
@@ -1,14 +1,14 @@
 function tweetCurrentPage(){
-	var title = document.getElementById('content').webNavigation.document.title;
-	if(title == null){
-		title = "";
-	}
-	var url = document.getElementById('content').webNavigation.currentURI.spec;
-	window.open(
-			'http://twitter.com/home?status=' +
-			encodeURIComponent(
-					title + " " + url
-			)
-	);
-	return void(0);
+    var title = document.getElementById('content').webNavigation.document.title;
+    if(title == null){
+	title = "";
+    }
+    var url = document.getElementById('content').webNavigation.currentURI.spec;
+    window.gBrowser.selectedTab = window.gBrowser.addTab(
+	'http://twitter.com/home?status=' +
+	    encodeURIComponent(
+		title + " " + url
+	    )
+    );
+    return void(0);
 }
