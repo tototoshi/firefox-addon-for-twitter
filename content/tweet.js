e768c52883482882c2b993184ea219ca73974efe
toshiyuki <toshiyuki.takahashi@hde.co.jp>
2011-09-01 13:05:10 +0900

    Added simple tweet menu

--- content/tweet.js
+++ content/tweet.js
@@ -1,24 +1,23 @@
-function tweetCurrentPage(){
-    var text = "";
+function getTweetURL (text) {
+    text = text || '';
+    var TWEET_INTENT_FORMAT = 'http://twitter.com/intent/tweet?source=webclient&text=%s';
+    return sprintf(TWEET_INTENT_FORMAT, text);
+}
 
-    var title = document.getElementById('content').webNavigation.document.title;
-    if(title == null){
-	title = "";
-    }
+function tweet() {
+    window.gBrowser.selectedTab = window.gBrowser.addTab(getTweetURL());
+    return void(0);
+}
 
-    var selection = content.getSelection().toString();
-    if (selection) {
-        text = selection;
-    } else {
-        text = title;
-    }
-
+function tweetCurrentPage() {
+    var title = document.getElementById('content').webNavigation.document.title || "";
+    var text = content.getSelection().toString() || title;
     var url = document.getElementById('content').webNavigation.currentURI.spec;
+
+    var TEXT_FORMAT = '"%s" %s';
     window.gBrowser.selectedTab = window.gBrowser.addTab(
-	'http://twitter.com/home?status=' +
-	    encodeURIComponent(
-		text + " " + url
-	    )
+        getTweetURL(encodeURIComponent(sprintf(TEXT_FORMAT, text, url)))
     );
+
     return void(0);
 }
